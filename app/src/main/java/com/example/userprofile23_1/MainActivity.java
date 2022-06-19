07c7030df4063fb0428f13900dac601c17d068e5
Outrun <401047902@qq.com>
2022-06-19 15:17:16 +0800

    初始化单词数据库

--- app/src/main/java/com/example/userprofile23_1/MainActivity.java
+++ app/src/main/java/com/example/userprofile23_1/MainActivity.java
@@ -34,14 +34,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void logout(View view) {
-//        String name = tvContent.getText().toString().trim();
-//
-//        Book book = new Book();
-//        book.setName(name);
-//        book.setIsSelected(0);
-//
-//        // 插入数据库中
-//        mMySQLiteOpenHelper.insertBook(book);
+
+        Book book1 = new Book();
+        book1.setName("character");
+        book1.setIsSelected(0);
+        mMySQLiteOpenHelper.insertBook(book1);
+
+        Book book2 = new Book();
+        book2.setName("character");
+        book2.setIsSelected(0);
+        mMySQLiteOpenHelper.insertBook(book2);
+
+        Book book3 = new Book();
+        book3.setName("character");
+        book3.setIsSelected(0);
+        mMySQLiteOpenHelper.insertBook(book3);
+
+        Book book4 = new Book();
+        book4.setName("character");
+        book4.setIsSelected(0);
+        mMySQLiteOpenHelper.insertBook(book4);
+
+        Book book5 = new Book();
+        book5.setName("character");
+        book5.setIsSelected(0);
+        mMySQLiteOpenHelper.insertBook(book5);
+
+        Book book6 = new Book();
+        book6.setName("character");
+        book6.setIsSelected(0);
+        mMySQLiteOpenHelper.insertBook(book6);
+
+        Book book7 = new Book();
+        book7.setName("character");
+        book7.setIsSelected(0);
+        mMySQLiteOpenHelper.insertBook(book7);
+
+        Book book8 = new Book();
+        book8.setName("character");
+        book8.setIsSelected(0);
+        mMySQLiteOpenHelper.insertBook(book8);
+
+        Book book9 = new Book();
+        book9.setName("character");
+        book9.setIsSelected(0);
+        mMySQLiteOpenHelper.insertBook(book9);
 
         mMySQLiteOpenHelper.initData(getResources());
     }
